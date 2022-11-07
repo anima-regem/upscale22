8bd5c095c41afb1f43734f517055e2ea7812c739
Anima Regem <vichukartha@gmail.com>
2022-11-08 01:54:18 +0530

    feat: add schedule component & correct icon naming issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import Banner from "./Components/Banner/Banner";
+import Banner from "./components/Banner/Banner";
 import "./App.css";
-import Webinar from "./Components/Webinar/Webinar";
-import { useState } from 'react'
 import Schedule from './components/schedule/Schedule'
 
 function App() {
@@ -11,7 +9,6 @@ function App() {
     <div className="App">
       <Banner />
       <Schedule/>
-      {/* <Webinar/> */}
     </div>
     );
 }
