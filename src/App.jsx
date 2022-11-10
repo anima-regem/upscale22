8c2e0422f6eb9749f5a14f081088cf88261c459b
hashirn <105742488+hashirn0811@users.noreply.github.com>
2022-11-10 22:18:36 +0530

    fix: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import WebinarPage from "./Pages/WebinarPage";
-import KnowMore from "./Pages/KnowMore";
-import webinarData from "./Data/Webinar.json";
+import { BrowserRouter} from "react-router-dom";
 import AnimatedRoutes from "./Components/AnimatedRoutes/AnimatedRoutes";
 
 
@@ -13,14 +9,6 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/webinar/:id"
-            element={<WebinarPage webinars={webinarData} />}
-          />
-          <Route path="/know-more" element={<KnowMore />} />
-        </Routes>
          <AnimatedRoutes />
       </BrowserRouter>
     </>
--- src/Components/AnimatedRoutes/AnimatedRoutes.jsx
+++ src/Components/AnimatedRoutes/AnimatedRoutes.jsx
@@ -5,6 +5,7 @@ import WebinarPage from "../../Pages/WebinarPage";
 import KnowMore from "../../Pages/KnowMore";
 import { AnimatePresence } from 'framer-motion'
 import ScrollToTop from '../../helpers/ScrollToTop';
+import webinarData from "../../Data/Webinar.json";
 
 function AnimatedRoutes() {
   const location = useLocation();
@@ -13,7 +14,10 @@ function AnimatedRoutes() {
           <ScrollToTop />
           <Routes key={ location.pathname } location={location}>
             <Route path="/" element={<Home />} />
-            <Route path="/webinar" element={<WebinarPage/>} />
+            <Route
+            path="/webinar/:id"
+            element={<WebinarPage webinars={webinarData} />}
+          />
             <Route path="/know-more" element={<KnowMore/>}/>
         </Routes>
     </AnimatePresence>
