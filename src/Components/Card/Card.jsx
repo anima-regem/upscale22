b8c8ce9c6e09485d9440c83871cdda9820a8590c
hashirn <105742488+hashirn0811@users.noreply.github.com>
2022-11-11 04:11:44 +0530

    fix: speaker grid styling

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Card.css'
 export default function Card({imgUrl, name, des, rounded}) {
   return (
-      <div className='card__container'>
+      <div className={rounded ? 'card__container rounded__card__container':'card__container'}>
           <div className="card__img__container">
               <img className={rounded ? 'img__rounded': null} src={imgUrl} alt={`${name}'s image`} />
           </div>
--- src/Components/SpeakerSection/SpeakerSection.jsx
+++ src/Components/SpeakerSection/SpeakerSection.jsx
@@ -6,10 +6,10 @@ import user from '../../assets/images/webinarimg.png'
 function SpeakerSection() {
     return (
         <div className='speakers__mentors'>
-            <div className="blur1"></div>
-            <div className="blur2"></div>
             <section className='speaker__section'>
-                <h1 className='speaker__heading'>Speakers</h1>
+                <h1 className='speaker__heading'>Speakers
+                     <div className="blur1"></div>
+                </h1>
                 <div className='speakers__container'>
                         <div className="speaker__el">
                             <Card imgUrl={user} name='User1' des="abcde"/>
@@ -18,18 +18,20 @@ function SpeakerSection() {
                             <Card imgUrl={user} name="User2" des="abcde"/>
                         </div>
                         <div className="speaker__el">
-                            <Card imgUrl={user} name="User4" des="abcde"/>
+                            <Card imgUrl={user} name="User3" des="abcde"/>
                         </div>
                         <div className="speaker__el user5">
-                            <Card imgUrl={user} name="User5" des="abcde"/>
+                            <Card imgUrl={user} name="User4" des="abcde"/>
                         </div>
                         <div className="speaker__el user6">
-                            <Card imgUrl={user} name="User6" des="abcde"/>
+                            <Card imgUrl={user} name="User5" des="abcde"/>
                         </div>
                 </div>
             </section>
             <section className='mentors__section'>
-                <h1 className='speaker__heading'>Mentors</h1>
+                <h1 className='speaker__heading mentor__heading'>Mentors
+                    <div className="blur2"></div>
+                </h1>
                 <div className="mentors__container">
                     <div className="mentor__el mentor_1">
                         <Card imgUrl={user} name="user1" rounded des="abcde"></Card>
