f0c37a2eb0e77f3c87b08bab3ecbcb2fa0f50a53
Antony Jaison <antonyjaison639@gmail.com>
2022-11-11 20:39:05 +0530

    feat:corrected ticket page

--- src/Pages/Speakers.jsx
+++ src/Pages/Speakers.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import Footer from '../Components/Footer/Footer'
-import Navbar from '../Components/Navbar/Navbar'
-import SpeakerSection from '../Components/SpeakerSection/SpeakerSection'
-import { motion } from 'framer-motion'
+import React from "react";
+import Footer from "../Components/Footer/Footer";
+import Navbar from "../Components/Navbar/Navbar";
+import SpeakerSection from "../Components/SpeakerSection/SpeakerSection";
+import { motion } from "framer-motion";
 function Speakers() {
   return (
     <motion.div
-    initial={{opacity: 0}}
-    animate={{ opacity: 1 }}
-    transition = {{ duration: 0.3, ease: "easeOut"}}
-    id='speakers'>
-          <Navbar />
-            <SpeakerSection />
-          <Footer />
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3, ease: "easeOut" }}
+      id="speakers"
+    >
+      <Navbar />
+      <SpeakerSection />
+      <Footer />
     </motion.div>
-  )
+  );
 }
 
-export default Speakers
+export default Speakers;
