7b71ae041f8da638fcf8ef06601057226461e9af
hashirn <105742488+hashirn0811@users.noreply.github.com>
2022-11-11 14:53:04 +0530

    fix: mentors styling

--- src/Components/SpeakerSection/SpeakerSection.jsx
+++ src/Components/SpeakerSection/SpeakerSection.jsx
@@ -38,14 +38,11 @@ function SpeakerSection() {
           <div className="blur2"></div>
         </h1>
         <div className="mentors__container">
-        {mentorData.map((mentor) => (
-            
-          // <div className="mentor__el mentor_1">
-          //   <Card imgUrl={mentor.img} name={mentor.name} rounded des={mentor.disc}></Card>
-          // </div>
-          <div></div>
-          ))}
-          
+        {/* {mentorData.map((mentor) => (
+          <div className="mentor__el mentor_1">
+            <Card imgUrl={mentor.img} name={mentor.name} rounded des={mentor.disc}></Card>
+          </div>
+          ))} */}
         </div>
       </section>
     </div>
